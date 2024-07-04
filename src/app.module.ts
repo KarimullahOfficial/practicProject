76177f65e0a8aa0bc90bfc0f5b66a45b73b58c43
karimullah <usamamalikdev18@gmail.com>
2024-07-04 18:22:58 +0500

    update the utlis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoadsModule } from './loads/loads.module';
 
 @Module({
-  imports: [],
+  imports: [LoadsModule],
   controllers: [AppController],
   providers: [AppService],
 })
