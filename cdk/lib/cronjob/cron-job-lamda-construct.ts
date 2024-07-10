bab7ffc4cfdfba16a1172bae8ec587d8c2f84960
karimullah <usamamalikdev18@gmail.com>
2024-07-10 13:32:01 +0500

    update the cron-job

--- cdk/lib/cronjob/cron-job-lamda-construct.ts
+++ cdk/lib/cronjob/cron-job-lamda-construct.ts
@@ -4,27 +4,28 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import { resolve } from "path";
- 
+
 export class NewToProgress extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id)
-        const env :IEnvironmentConfig = scope.node.tryGetContext(enviroment)
-        const { stage, APP_NAME,MongoUri,DataBaseName} = env
-        const cronJob = new lambda.Function(this,`${APP_NAME} - ${stage}-cron-job-lamda-cron-job`,{
-            runtime:lambda.Runtime.NODEJS_20_X,
-            handler:'main.cronjob-handler',
-            code:lambda.Code.fromAsset(resolve(__dirname ,'../bulid')),
-            environment:{
+
+        const env: IEnvironmentConfig = scope.node.tryGetContext(enviroment)
+        const { stage, APP_NAME, MongoUri, DataBaseName } = env
+
+        const cronJob = new lambda.Function(this, `${APP_NAME}-${stage}-cron-job-lambda`, {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: 'main-handler',
+            code: lambda.Code.fromAsset(resolve(__dirname, '///')),
+            environment: {
                 NODE_PATH: '$NODE_PATH:/opt',
-                MongoUri,DataBaseName
+                MongoUri,
+                DataBaseName
             }
-        
-             
+
         })
-        const rule = new events.Rule(this,'CRON-JOB',{
-schedule: events.Schedule.cron({minute:'',hour:'',day:'',month:'',year:''})
+        const rule = new events.Rule(this, '', {
+            schedule: events.Schedule.cron({ minute: '' })
         })
         rule.addTarget(new targets.LambdaFunction(cronJob))
     }
-    
 }
